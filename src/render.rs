efe1f963330e43d26f6996a5118fa3699ac1ffbd
Phoomparin Mano <work@phoom.in.th>
2018-01-13 21:10:05 +0700

    :sparkles: Added Blue Window Example

--- src/render.rs
+++ src/render.rs
@@ -1,12 +1,20 @@
 extern crate glium;
 
-use self::glium::Display;
+use self::glium::{Display, Surface};
 use self::glium::glutin::*;
 
-fn handle_events(events_loop: &mut EventsLoop) {
+fn render(display: &Display) {
+  let mut target = display.draw();
+  target.clear_color(0.0, 0.0, 1.0, 1.0);
+  target.finish().unwrap();
+}
+
+fn handle_events(display: &Display, events_loop: &mut EventsLoop) {
   let mut closed = false;
 
   while !closed {
+    render(&display);
+
     events_loop.poll_events(|ev| {
       match ev {
         Event::WindowEvent { event, .. } => {
@@ -21,7 +29,7 @@ fn handle_events(events_loop: &mut EventsLoop) {
   }
 }
 
-fn create_window() -> Display {
+pub fn init() {
   let mut events_loop = EventsLoop::new();
 
   let window = WindowBuilder::new()
@@ -31,12 +39,6 @@ fn create_window() -> Display {
   let context = ContextBuilder::new();
   let display = Display::new(window, context, &events_loop).unwrap();
 
-  handle_events(&mut events_loop);
-
-  display
-}
-
-pub fn init() {
-  let display = create_window();
+  handle_events(&display, &mut events_loop);
 }
 
