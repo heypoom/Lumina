93107cd6710a2024ac10aec12a3cc7f89145a52d
Phoomparin Mano <work@phoom.in.th>
2018-01-13 19:23:14 +0700

    :tada: Initialized Repository

---
+++ src/coord.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+pub struct Coord {
+    x: u32,
+    y: u32,
+    z: u32
+}
+
+impl Coord {
+    pub fn new(x: u32, y: u32, z: u32) -> Coord {
+        Coord {x, y, z}
+    }
+
+    pub fn blank() -> Coord {
+        Coord {x: 0, y: 0, z: 0}
+    }
+}
